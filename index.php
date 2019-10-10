4750d64d45341818cb962f32ff51b2ba3c402b8c
ComandPromt <vayase_a_ferrol@hotmail.com>
2019-10-10 14:00:38 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,64 +1,81 @@
 <?php
 
-if(!file_exists ("uploads")) {
-	mkdir("uploads");
-}
+session_start();
 
-if(isset($_POST['subir'])){
+include('funciones.php');
 
-	$uploadedfile_size=$_FILES['uploadedfile'][size];
-	
-	$file_name=$_FILES[uploadedfile][name];
-	
-	$add="uploads/$file_name";
-	
-	if(move_uploaded_file ($_FILES[uploadedfile][tmp_name], $add)){
-	
-		rename($add,'uploads/test.'.substr($file_name,-3));
-		
-		include('funciones.php');
-	
-		if(file_exists('test.png')){
-			png_a_jpg('test.png');
-		}
-		
-		$url='uploads/test.jpg';
-		
-		if(file_exists($url)){
-			
-			if(count(getColorPallet($url))==0){
-				deliver_response(200, true);
-			}
-		
-			else{
-				deliver_response(200, false);
-			}
-			
-			unlink($url);
-		}
-		
-		else{
-			deliver_response(400, false);
-		}
-	
+if(!isset($_SESSION['respuesta'])){
+
+	if(!file_exists ("uploads")) {
+		mkdir("uploads");
 	}
+	
+	if(isset($_POST['subir'])){
 
+		$uploadedfile_size=$_FILES['uploadedfile'][size];
+	
+		$file_name=$_FILES[uploadedfile][name];
+	
+		$add="uploads/$file_name";
+	
+		if(move_uploaded_file ($_FILES[uploadedfile][tmp_name], $add)){
+
+			rename($add,'uploads/test.'.substr($file_name,-3));
+		
+			if(file_exists('test.png')){
+				png_a_jpg('test.png');
+			}
+		
+			$url='uploads/test.jpg';
+		
+			if(file_exists($url)){
+			
+				if(count(getColorPallet($url))==0){
+					
+					$_SESSION['respuesta']=true;
+				
+				}
+		
+				else{
+					
+					$_SESSION['respuesta']=false;
+						
+				}
+				
+				unlink($url);
+				
+				echo '<script>location.href="resultado.php?respuesta='.$_SESSION['respuesta'].'";</script>';
+				
+			}
+	
+			else{
+				echo '<script>location.href="resultado.php";</script>';
+			}
+			
+		}
+	
+		else{
+			echo '<script>location.href="resultado.php";</script>';
+		}
+
+	}
+	
+	print '<div style="margin:auto;text-align:center;">
+		
+			<form enctype="multipart/form-data" action="'.$_SERVER['PHP_SELF'].'" method="post">
+				
+				<p>
+					<input name="uploadedfile" type="file" />
+				</p>
+				
+				<p>
+					<input name="subir" type="submit" value="upload"/>
+				</p>
+			
+			</form>
+		
+		</div>';
+	
 }
 
 ?>
-
-<div style="margin:auto;text-align:center;">
-
-	<form enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-		
-		<p>
-			<input name="uploadedfile" type="file" />
-		</p>
-		
-		<p>
-			<input name="subir" type="submit" value="upload"/>
-		</p>
-	
-	</form>
-
-</div>
